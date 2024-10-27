aa44d8fe29b2a2c9f685d78286b61e82b72ddc99
Sagar Makode <131523035+sagar-makode@users.noreply.github.com>
2024-10-28 03:12:42 +0530

    Update fetchBlogs.js

--- src/fetchblogs/fetchBlogs.js
+++ src/fetchblogs/fetchBlogs.js
@@ -1,5 +1,5 @@
 export async function fetchBlogs() {
-    const res = await fetch("http://127.0.0.1:3000/api/blog", {cache: "no-store",});
+    const res = await fetch("https://form-solution.vercel.app/api/blog", {cache: "no-store",});
     let data = await res.json()
     return data
 }
