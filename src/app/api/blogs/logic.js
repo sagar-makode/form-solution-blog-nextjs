35df73cf38a2d18a309d5e1c13131d505d52f61a
sagar-makode <makodepatil1@gmail.com>
2024-10-28 02:56:23 +0530

    added server

--- src/app/api/blogs/logic.js
+++ src/app/api/blogs/logic.js
@@ -1,5 +1,5 @@
 export async function fetchAllBlogs() {
-  const res = await fetch("http://localhost:3000/api/blogs");
+  const res = await fetch("http://127.0.0.1:3000/api/blogs");
   
 
   if (!res.ok) {
