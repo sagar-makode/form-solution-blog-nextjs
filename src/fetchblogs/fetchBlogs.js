fbc0d2495a22659c7edaec8759309322d9b62f75
sagar-makode <makodepatil1@gmail.com>
2024-10-28 11:39:39 +0530

    added error log

--- src/fetchblogs/fetchBlogs.js
+++ src/fetchblogs/fetchBlogs.js
@@ -1,5 +1,17 @@
 export async function fetchBlogs() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/blogs`, {cache: "no-store",});
-    let data = await res.json()
-    return data
+
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/blogs`, {cache: "no-store",});
+        if (!res.ok) {
+            const errorText = await res.text();
+            console.error("Failed to fetch:", res.status, res.statusText, errorText);
+            throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
+        }
+        let data = await res.json()
+        return data
+        
+    } catch (error) {
+        console.error("Error fetching blogs:", error);
+        return null;
+    }
 }
