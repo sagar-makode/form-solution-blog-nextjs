7c8daf28da6dc94225aaeb9f5db5311702776f5c
sagar-makode <makodepatil1@gmail.com>
2024-10-28 11:02:30 +0530

    added config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['blogger.googleusercontent.com','res.cloudinary.com'], // Add your domain here
     },
+    eslint:{
+      ignoreDuringBuilds :true,
+    }
   };
 
 export default nextConfig;
