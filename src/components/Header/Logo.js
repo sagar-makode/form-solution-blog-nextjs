dda9f3e2a997dfc0ed025c2b2ed407fba4e38e2d
sagar-makode <makodepatil1@gmail.com>
2024-11-04 01:00:51 +0530

    change CodeBucks logo alt

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
         <div className=" w-12 md:w-16 rounded-full overflow-hidden  border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={channelImg || null} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
+            <Image src={channelImg || null} alt="formsolution logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
         <span className="font-bold dark:font-semibold text-lg md:text-xl">Form Solution</span>
     </Link>
